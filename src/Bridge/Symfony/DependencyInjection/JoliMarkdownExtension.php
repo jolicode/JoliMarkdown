536f559eccdfa403671e6b5a9401074b209977e1
Xavier Lacot <xavier@lacot.org>
2025-12-15 16:31:32 +0100

    Remove usage of the deprecated HttpKernel base extension class

--- src/Bridge/Symfony/DependencyInjection/JoliMarkdownExtension.php
+++ src/Bridge/Symfony/DependencyInjection/JoliMarkdownExtension.php
@@ -13,8 +13,8 @@ namespace JoliMarkdown\Bridge\Symfony\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class JoliMarkdownExtension extends Extension
 {
